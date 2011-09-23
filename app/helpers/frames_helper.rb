b5da78ee6fc90128ba9e0421f1acd69f91ef574b
Thomas Ritchie <halthe5th@gmail.com>
2011-09-23 11:13:53 -0600

    no more nils! part done

--- app/helpers/frames_helper.rb
+++ app/helpers/frames_helper.rb
@@ -3,11 +3,21 @@ module FramesHelper
     box1, box2 = @frame.ball1, @frame.ball2
     if @frame.ball1 == 10
       box1 = nil; box2 = 'X'
-    elsif @frame.ball1.to_i + @frame.ball2.to_i == 10
+    elsif @frame.ball1 + @frame.ball2 == 10
       box2 = '/'
+    else
+      box1 = '-' if @frame.ball1 == 0
+      box2 = '-' if @frame.ball2 == 0
     end
-    box1 = '-' if @frame.ball1 == 0
-    box2 = '-' if @frame.ball2 == 0
     return [box1, box2]
   end
+
+  def finished?
+    if frame.id == 10
+      return true if @frame.ball1 == 10 or @frame.ball2 != nil
+    else
+      return false if finished(Frame.find(@frame.id+1))
+    end
+  end
+
 end
