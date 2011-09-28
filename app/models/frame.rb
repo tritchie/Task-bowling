6d7f0509e78fdb28da0d7aa1474d88e7f35d3b41
Thomas Ritchie <halthe5th@gmail.com>
2011-09-28 11:21:02 -0600

    ball1 label

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,10 +1,12 @@
 class Frame < ActiveRecord::Base
 
   belongs_to :game
-  validates :ball1, :numericality => {:only_integer => true}
-  validates :ball2, :numericality => {:only_integer => true}
+  validates :ball1, :numericality => {:only_integer => true, :allow_nil => true}
+  validates :ball2, :numericality => {:only_integer => true, :allow_nil => true}
 
   validates_each :ball2 do |model, attr, value|
+    model.ball1 = 0 if model.ball1 == nil
+    model.ball2 = 0 if model.ball2 == nil
     if (model.ball1 + model.ball2) > 10
       model.errors.add("There's only 10 pins! #{model.ball1}, #{model.id}, #{model.ball2}") 
     end
